c19e726eb8b2f3845b8f49e89d16706aaf55b34a
RhakezS <rhakez2077@gmail.com>
2026-09-03 12:17:20 +0700

    pekan1_2611532013

---
+++ pekan 1/program1_2611532013.py
@@ -0,0 +1,12 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello world")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh Rhakez Elvano Stonie" + "2611532013")
